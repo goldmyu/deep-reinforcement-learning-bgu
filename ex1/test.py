7ef4ae30b8237d7a1432a60920f6bfb59168ec31
juliatul <38453508+juliatul@users.noreply.github.com>
2019-11-27 11:58:53 +0200

    floopy

--- ex1/test.py
+++ ex1/test.py
@@ -10,19 +10,19 @@ learning_rate = 0.1
 discount_factor = 0.99
 epsilon_greedy_rate = 0.2
 epsilon_greedy_decay_rate = 0.005
+# Create the q-value lookup table in the size of states X actions and init it to zerosv
+q_value_lookup_table = np.zeros(shape=[16, 4])
+
 reward_history = []
 
 # ======================================================================================================================
 
 # Create the environment
 env = gym.make('FrozenLake-v0')
-observation = env.reset()
-
-# Create the q-value lookup table in the size of states X actions and init it to zeros
-q_value_lookup_table = np.zeros(shape=[16, 4])
 
 
-def choose_step_by_epsilon_greedy(state):
+
+def choose_action_by_epsilon_greedy(state):
     rand_num = np.random.uniform(0,1)
     if rand_num <= epsilon_greedy_rate:
         _step_to_take = env.action_space.sample()
@@ -31,22 +31,25 @@ def choose_step_by_epsilon_greedy(state):
     return _step_to_take
 
 
-def update_q_table(state, next_state, action, _reward):
-    q_value_lookup_table[state,action] = q_value_lookup_table[state,action] + \
-                                         learning_rate*(_reward +
-                                                        discount_factor * np.argmax(q_value_lookup_table[next_state]) -
-                                                        q_value_lookup_table[state,action])
 
 
+def update_q_table(state, action, target):
+    q_value_lookup_table[state,action] = (1 - learning_rate) * q_value_lookup_table[state,action] + \
+                                         learning_rate * target
 for episode in range(train_episodes):
+    state = env.reset()
     for step in range(max_steps_per_episode):
         env.render()
-        step_to_take = choose_step_by_epsilon_greedy(observation)
-        observation, reward, done, info = env.step(step_to_take)  # take a random action
-
+        action = choose_action_by_epsilon_greedy(state)
+        next_state, reward, done, info = env.step(action)  # take a random action
         if done:
-
-        update_q_table()
+            target = reward
+            update_q_table(state, action, target)
+            break
+        else:
+            target = reward + discount_factor * np.argmax(q_value_lookup_table[next_state])
+            update_q_table(state, action, target)
+        state = next_state
 
 
 
