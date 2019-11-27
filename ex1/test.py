051cc0f3ab260940cf90e6ff810258c522a4d3fd
Yuval Goldman1 <goldman1yuval@gmail.com>
2019-11-27 16:23:13 +0200

    every step is an improvement 2

--- ex1/test.py
+++ ex1/test.py
@@ -1,5 +1,6 @@
 import gym
 import numpy as np
+import tensorboard as tb
 
 # ======================================================================================================================
 
@@ -7,11 +8,11 @@ train_episodes = 5000
 max_steps_per_episode = 100
 
 learning_rate = 0.1
-discount_factor = 0.85
+discount_factor = 0.99
 success_counter = 0
 
 epsilon_greedy_rate = 0.9
-epsilon_greedy_decay_rate = 0.999
+epsilon_greedy_decay_rate = 0.99
 min_epsilon = 0.1
 
 reward_history = []
@@ -31,15 +32,15 @@ def choose_action_by_epsilon_greedy(_state):
     if rand_num <= epsilon_greedy_rate:
         _step_to_take = env.action_space.sample()
     else:
-        all_max_indexes = np.argwhere(q_value_lookup_table[state] == np.amax(q_value_lookup_table[state]))
+        all_max_indexes = np.argwhere(q_value_lookup_table[_state] == np.amax(q_value_lookup_table[_state]))
         np.random.shuffle(all_max_indexes)
         _step_to_take = all_max_indexes.item(0)
     return _step_to_take
 
 
 def update_q_table(_state, _action, _target):
-    q_value_lookup_table[state, action] = (1 - learning_rate) * q_value_lookup_table[state, action] + \
-                                         learning_rate * target
+    q_value_lookup_table[_state, _action] = (1 - learning_rate) * q_value_lookup_table[_state, _action] + \
+                                            (learning_rate * _target)
 
 
 for episode in range(train_episodes):
@@ -54,14 +55,18 @@ for episode in range(train_episodes):
             if next_state == 15:
                 success_counter += 1
             target = reward
+            reward_history.append(reward)
             update_q_table(state, action, target)
+            env.render()
             break
         else:
-            target = reward + discount_factor * np.argmax(q_value_lookup_table[next_state])
+            max_action = np.argmax(q_value_lookup_table[next_state])
+            target = reward + discount_factor * q_value_lookup_table[next_state, max_action]
             update_q_table(state, action, target)
-        env.render()
         state = next_state
+        env.render()
 
 print("number of success is {}".format(success_counter))
 print("Q-value table is {}".format(q_value_lookup_table))
+print("Reward history is {}".format(reward_history))
 env.close()
