d4e136c464732d4facca990426a7aa71d159d699
Yuval Goldman1 <goldman1yuval@gmail.com>
2019-12-17 18:00:56 +0200

    works

--- ex2/actor_critic.py
+++ ex2/actor_critic.py
@@ -2,6 +2,8 @@ import gym
 import numpy as np
 import tensorflow as tf
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 # =========================================== Define hyperparameters ===================================================
 
